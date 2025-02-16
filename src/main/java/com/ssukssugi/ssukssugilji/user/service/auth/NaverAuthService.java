017c65d77afeabea58c1306310a009dd8cab0793
jungh8n <jh981113@naver.com>
2025-02-16 17:16:27 +0900

    fix: delete naver response validation

--- src/main/java/com/ssukssugi/ssukssugilji/user/service/auth/NaverAuthService.java
+++ src/main/java/com/ssukssugi/ssukssugilji/user/service/auth/NaverAuthService.java
@@ -5,7 +5,6 @@ import com.ssukssugi.ssukssugilji.user.dto.LoginType;
 import com.ssukssugi.ssukssugilji.user.dto.SocialAuthUserInfoDto;
 import com.ssukssugi.ssukssugilji.user.dto.naver.NaverResponse;
 import com.ssukssugi.ssukssugilji.user.dto.naver.NaverUserInfoResponse;
-import java.util.Objects;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,7 +27,6 @@ public class NaverAuthService implements SocialAuthService {
     @Override
     public SocialAuthUserInfoDto getAuthUserInfo(String accessToken) {
         NaverUserInfoResponse response = naverWebClientProxy.getUserInfo(accessToken);
-        validateResponse(response);
         NaverResponse userInfo = response.getResponse();
 
         return SocialAuthUserInfoDto.builder()
@@ -36,12 +34,5 @@ public class NaverAuthService implements SocialAuthService {
             .emailAddress(userInfo.getEmail())
             .loginType(LOGIN_TYPE)
             .build();
-    }
-
-    public void validateResponse(NaverUserInfoResponse response) {
-        if (!Objects.equals(response.getResultCode(), "200")) {
-            throw new RuntimeException(String.format("Naver API failed with code %s, message: %s",
-                response.getResultCode(), response.getMessage()));
-        }
     }
 }
