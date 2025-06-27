52238c8987c6739858007a9835c5dae627b59fc0
jungh8n <jh981113@naver.com>
2025-06-27 21:22:46 +0900

    fix: make plantEnvironment elements nullable

--- src/main/java/com/ssukssugi/ssukssugilji/plant/dto/PlantEnvironment.java
+++ src/main/java/com/ssukssugi/ssukssugilji/plant/dto/PlantEnvironment.java
@@ -1,5 +1,6 @@
 package com.ssukssugi.ssukssugilji.plant.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
@@ -9,6 +10,8 @@ public class PlantEnvironment {
 
     @Min(1)
     @Max(4)
+    @Nullable
     private Short shine;
+    @Nullable
     private Place place;
 }
