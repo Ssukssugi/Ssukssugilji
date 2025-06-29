c1cc3d119591a7f003595a222f021bdb816ef6d4
jungh8n <jh981113@naver.com>
2025-06-29 23:36:43 +0900

    fix: order plantList by createdAt desc

--- src/main/java/com/ssukssugi/ssukssugilji/plant/service/PlantService.java
+++ src/main/java/com/ssukssugi/ssukssugilji/plant/service/PlantService.java
@@ -1,11 +1,13 @@
 package com.ssukssugi.ssukssugilji.plant.service;
 
 import com.ssukssugi.ssukssugilji.auth.service.SecurityUtil;
+import com.ssukssugi.ssukssugilji.common.BaseEntity;
 import com.ssukssugi.ssukssugilji.plant.dao.PlantRepository;
 import com.ssukssugi.ssukssugilji.plant.dto.UserPlantCreateRequest;
 import com.ssukssugi.ssukssugilji.plant.dto.UserPlantDto;
 import com.ssukssugi.ssukssugilji.plant.entity.Plant;
 import com.ssukssugi.ssukssugilji.user.entity.User;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,7 @@ public class PlantService {
     public List<UserPlantDto> getUserPlantList(User user) {
         return plantRepository.findByUser(user)
             .stream()
+            .sorted(Comparator.comparing(BaseEntity::getCreatedAt).reversed())
             .map(UserPlantDto::fromEntity)
             .collect(Collectors.toList());
     }
