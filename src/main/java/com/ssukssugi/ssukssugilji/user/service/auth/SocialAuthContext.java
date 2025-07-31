68780312d771e9b3c7177e0fadaadaefc12147c3
jungh8n <jh981113@naver.com>
2025-07-31 23:13:55 +0900

    misc: improve log

--- src/main/java/com/ssukssugi/ssukssugilji/user/service/auth/SocialAuthContext.java
+++ src/main/java/com/ssukssugi/ssukssugilji/user/service/auth/SocialAuthContext.java
@@ -8,10 +8,12 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SocialAuthContext {
 
     private final List<SocialAuthService> socialAuthServices;
@@ -24,6 +26,7 @@ public class SocialAuthContext {
     }
 
     public SocialAuthUserInfoDto getAuthUserInfo(LoginType loginType, String accessToken) {
+        log.info("Fetching user info for login type: {}, accessToken: {}", loginType, accessToken);
         SocialAuthService socialAuthService = socialAuthServiceMap.get(loginType);
         if (socialAuthService == null) {
             throw new IllegalArgumentException("Invalid login type");
